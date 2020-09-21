c300ce5a38fd9423daebf5766bbb55779e59229e
Krishna Mohan Shah <krishna.mohan.shah@nmbu.no>
2020-09-21 12:47:05 +0545

    Day 6: Complete App with icons

--- lib/GetLocation.dart
+++ lib/GetLocation.dart
@@ -1,26 +1,13 @@
 import 'package:geolocator/geolocator.dart';
 
 class Getlocation{
-  //get current location
-  double latitude;
-  double longitude;
-  String city;
-  Future<void> getCurrentLocation() async{
+  //Get current location
+  Future getCurrentLocation() async{
     try{
       Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-      latitude = position.latitude;
-      longitude= position.longitude;
-      city = await getCityName(position.latitude, position.longitude);
+      return position;
     }catch(e){
       print(e);
     }
   }
-
-  Future<String> getCityName(double lat, double lon) async{
-    List<Placemark> placemark = await Geolocator().placemarkFromCoordinates(lat, lon);
-    print('City Name: ${placemark[0].locality}');
-    return placemark[0].locality;
-  }
-
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:weatherapp/GetLocation.dart';
-import 'dart:convert' as convert;
-import 'package:http/http.dart' as http;
+import 'DisplayWeatherData.dart';
 
 void main() {
   runApp(WeatherApp());
@@ -14,12 +12,7 @@ class WeatherApp extends StatefulWidget {
 }
 
 class _WeatherAppState extends State<WeatherApp> {
-  String apikey='6554060f5df3f93d1ee26accc733c80e';
-  var description;
-  var temp;
-  String city;
-
-
+  DisplayWeatherData _displayWeatherData=DisplayWeatherData();
   @override
   Widget build(BuildContext context) {
 
@@ -28,67 +21,10 @@ class _WeatherAppState extends State<WeatherApp> {
         body: Column(
           children: [
             Container(
-              child:Image.asset('images/dayTime.jpg'),
+              child: displayImage(),
             ),
             Container(
-              margin: EdgeInsets.only(top: 50.0),
-              child: Text('You are in:', style:
-                TextStyle(
-                  fontSize: 35.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.lightBlue[500],
-                ),),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 35.0),
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    child: Text('$city',style:
-                    TextStyle(
-                        fontSize: 35.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.indigo
-                    ) ,),
-                  ),
-                  SizedBox(width: 10.0,),
-                  Container(
-                    child: Icon(
-                      Icons.location_on,
-                      color: Colors.redAccent,
-                    ),
-                  ),
-                ],
-            ),
-            ),
-            Card(
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              color: Colors.white,
-              child: ListTile(
-                leading: Icon(
-                    Icons.wb_sunny,
-                    color: Colors.amber,
-                ),
-                title: Text(
-                  'Temperature: ${temp.toString()} C'
-                ),
-                subtitle: Text('Staus= ${description.toString()}'),
-              ),
-            ),
-            Container(
-              child: Center(
-                child: FlatButton(
-                  child: Text('Get weather info'),
-                  color: Colors.lightBlue[500],
-                  textColor: Colors.white,
-                  onPressed: (){
-                    setState(() {
-                      getLocation();
-                    });
-                  },
-                )
-              )
-
+              child: _displayWeatherData,
             ),
           ],
         )
@@ -108,27 +44,6 @@ class _WeatherAppState extends State<WeatherApp> {
     }
   }
 
-  void getLocation() async{
-    Getlocation getlocation = Getlocation();
-    await getlocation.getCurrentLocation();
-
-    print(getlocation.latitude);
-    print(getlocation.longitude);
-    print(getlocation.city);
-    city=getlocation.city;
-    getTemp(getlocation.latitude,getlocation.longitude);
-  }
-  //Get current Temperature
-  Future<void> getTemp(double lat,double lon) async{
-    http.Response response = await http.get('api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=${apikey}&units=metric');
-    print(response.body);
-
-    var dataDecoded = convert.jsonDecode(response.body);
-    description = dataDecoded['weather'][0]['description'];
-    print(description);
-    temp = dataDecoded['main']['temp'];
-    print(temp);
-}
 
 }
 
